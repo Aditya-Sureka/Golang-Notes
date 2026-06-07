fc52a04abfb96a102057d62bfda0d36c370072d0
Aditya <adityasureka@7.in>
2026-06-07 18:55:47 +0530

    Simple Values in Go

--- L-2_Simple_Values/main.go
+++ L-2_Simple_Values/main.go
@@ -5,5 +5,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(1)
+	fmt.Println(1 + 1) // Integer addition
+	fmt.Println("hello golang") // String literal
+	fmt.Println(true) // Boolean value
+	fmt.Println(3.14) // Floating-point number
 }
